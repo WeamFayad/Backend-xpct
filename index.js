31a7595cb21cd37c8637f39c64c85176b2ce60dd
weamfayad <weamfayad@outlook.com>
2024-04-19 13:45:34 +0300

    adding the indexjs and env

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('./configs/database.config');
+const userRouter = require('./routes/user.routes');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use('/users', userRouter);
+
+app.listen(8000, () => {
+  console.log('Server started on port 8000');
+});
